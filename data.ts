e3748ed9fb39cd07f6b16a62d64c5960f07f37ed
Kygan <kygandao23@gmail.com>
2022-01-10 18:23:57 -0800

    added more detail in about me

--- data.ts
+++ data.ts
@@ -2,39 +2,39 @@
 
 import { BsAward, BsChatRightText, BsStack } from 'react-icons/Bs'
 import { GoGraph } from 'react-icons/go'
-import { MdLightbulbOutline, MdOutlineManageSearch } from 'react-icons/md'
+import { MdLightbulbOutline } from 'react-icons/md'
 import { RiTeamLine } from 'react-icons/ri'
 import { IOffer } from './type'
 
 export const services:IOffer[] = [
     {
-        title:'Application Creation and Debugging',
-        about: 'details about application creation',
+        title:'Resourceful and Adaptable',
+        about: 'Unafraid to journey into the unknown and learn more about what is needed to complete a task or solve a problem. "Whichever the Stack, I will adapt"',
         Icon: BsStack,
     },
     {
         title:'Project Management',
-        about: 'details about project management',
+        about: 'Authenticity with a "people" approach which inspires my team and instills confidence.',
         Icon: GoGraph,
     },
     {
         title:'Creativity',
-        about: 'details about creative tools',
+        about: 'The ability to think about and solve problems in a new or different way',
         Icon: MdLightbulbOutline,
     },
     {
         title:'Professionalism',
-        about: 'details about workplace professionalism',
+        about: '10+ years of professional experience in more than two unique fields',
         Icon: BsAward,
     },
     {
         title:'Collaborative Mindset',
-        about: 'details about teamwork',
+        about: 'A willingness to try mulitple strategies at the same time, work through conflicts, and a focus on "We" rather than "Me"',
         Icon: RiTeamLine,
     },
     {
         title:'Communication',
-        about: 'details about strong communication',
+        about: 'Exceptional verbal and written communication, delivered in a clear and concise manner',
         Icon: BsChatRightText,
     }
 ]
