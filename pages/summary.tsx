5110691ea83dce9de06c480561616078c2bdc8d2
Kygan <kygandao23@gmail.com>
2022-01-14 20:21:25 -0800

    finalized layout of summary page

--- pages/summary.tsx
+++ pages/summary.tsx
@@ -3,26 +3,36 @@ import { languages, tools } from "../data"
 
 const summary = () => {
     return (
-        <div className='px-6 py-2'>
-            <div className='grid gap-5 md:grid-cols-2'>
+        <div className='px-6 py-2 '>
+            <div className='grid gap-5 px-4 pt-1 bg-gray-300 md:grid-cols-2' style={{marginLeft:'-1rem', marginRight:'-1rem'}}>
                 <div id='education'>
                     <h5 className='my-2 text-lg font-semibold text-green-900 font-rock'>Education</h5>
                     <div>
-                        <h5 className='my-2 text-lg font-semibold'>Cognizant ProActive Java Boot Camp</h5>
-                        <p className='my-1'>Southern Methodist University - Expected March 2022</p>
-                        <h5 className='my-2 text-lg font-semibold'>Boot Camp Certificate: Web Development</h5>
-                        <p className='my-1'>University of Oregon</p>
-                        <h5 className='my-2 text-lg font-semibold'>Graphic Design</h5>
-                        <p className='my-1'>The Art Institutes International Minnesota</p>
+                        <div className='p-1 my-1 space-x-5 bg-gray-200 rounded-md'>
+                            <h5 className='my-1 font-semibold'>Cognizant ProActive Java Boot Camp</h5>
+                            <p>Southern Methodist University - Expected March 2022</p>
+                        </div>
+                        <div className='p-1 my-1 space-x-5 bg-gray-200 rounded-md'>
+                            <h5 className='my-1 font-semibold'>Boot Camp Certificate: Web Development</h5>
+                            <p>University of Oregon</p>
+                        </div>
+                        <div className='p-1 my-1 space-x-5 bg-gray-200 rounded-md'>
+                            <h5 className='my-1 font-semibold'>Graphic Design</h5>
+                            <p>The Art Institutes International Minnesota</p>
+                        </div>
                     </div>
                 </div>
                 <div id='experience'>
                     <h5 className='my-2 text-lg font-semibold text-green-900 font-rock'>Work History</h5>
                     <div>
-                        <h5 className='my-2 text-lg font-semibold'>Insurance Account Representative</h5>
-                        <p className='my-1'>State Farm Insurance 2016 April - Current</p>
-                        <h5 className='my-2 text-lg font-semibold'>Lead Utility Locator</h5>
-                        <p className='my-1'>USIC, LLC 2012 January - 2016 April</p>
+                        <div className='p-1 my-1 space-x-5 bg-gray-200 rounded-md'>
+                            <h5 className='my-1 font-semibold'>Insurance Account Representative</h5>
+                            <p>State Farm Insurance 2016 April - Current</p>
+                        </div>
+                        <div className='p-1 my-1 space-x-5 bg-gray-200 rounded-md'>
+                            <h5 className='my-1 font-semibold'>Lead Utility Locator</h5>
+                            <p>USIC, LLC 2012 January - 2016 April</p>
+                        </div>
                     </div>
                 </div>
             </div>
