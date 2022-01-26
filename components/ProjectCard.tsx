95fd58411cc2b776bbcdcb658db4230b72d344f8
Kygan <kygandao23@gmail.com>
2022-01-25 18:28:47 -0800

    completed project card functionality, begin project nav and filtering

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -29,17 +29,17 @@ const ProjectCard:FunctionComponent<{
             showProjectDetails && (
 
 
-        <div id='leftSide' className='grid md:grid-cols-2'>
+        <div id='leftSide' className='absolute top-0 left-0 z-10 grid w-full h-auto p-2 text-black bg-gray-300 rounded-lg md:grid-cols-2 gap-x-6 dark:bg-gray-800 dark:text-white'>
             <div>
-                <img src={image_path} alt={name}/>
-                <div>
-                    <a href={github_url}>
+                <img src={image_path} alt={name} className='rounded-lg'/>
+                <div className='flex justify-center my-4 space-x-2'>
+                    <a href={github_url} className='flex items-center px-3 py-1 space-x-2 text-green-900 hover:text-green-600 dark:text-white dark:hover:text-green-600'>
                         <GrGithub />
                         <span>
                             GitHub
                         </span>
                     </a>
-                    <a href={deployed_url}>
+                    <a href={deployed_url} className='flex items-center px-3 py-1 space-x-2 text-green-900 hover:text-green-600 dark:text-white dark:hover:text-green-600'>
                         <CgWebsite />
                         <span>
                             Site
@@ -50,18 +50,18 @@ const ProjectCard:FunctionComponent<{
         
 
         <div id='rightSide'>
-            <h3>{name}</h3>
-            <h4>{description}</h4>
-            <div>
+            <h3 className='mb-2 text-green-900 lg:text-2xl dark:text-white md:text-lg sm:text-lg'>{name}</h3>
+            <p className='mb-2 text-base'>{description}</p>
+            <div className='flex flex-wrap mt-5 space-x-2 text-sm'>
                 {
                     tech_tags.map(tech => 
-                    <span key={tech}>
+                    <span key={tech} className='px-2 py-1 my-1 bg-gray-200 rounded-xl dark:bg-gray-700'>
                         {tech_tags}
                     </span>)
                 }
             </div>
             <button>
-                <CgCloseO size={20} className='text-green-900' onClick={() => setShowProjectDetails(false)}/>
+                <CgCloseO size={20} className='absolute text-green-900 top-2 right-2 hover:text-green-600' onClick={() => setShowProjectDetails(false)}/>
             </button>
         </div>
     </div>
--- pages/projects.tsx
+++ pages/projects.tsx
@@ -3,14 +3,14 @@ import { projects } from "../data"
 
 const Projects = () => {
     return (
-        <div className='p-4 text-xl'>
+        <div className='p-4 px-5 py-2 overflow-y-scroll text-xl' style={{height:'65vh' }}>
             <nav>
                 Navbar
             </nav>
-            <div className='grid grid-cols-12 gap-3 my-3'>
+            <div className='relative grid grid-cols-12 gap-3 my-3'>
                 {
                 projects.map(project => (
-                    <div className='col-span-12 p-2 lg:col-span-4 sm:col-span-6 '>
+                    <div className='col-span-12 p-2 bg-gray-300 rounded-lg dark:bg-gray-800 lg:col-span-4 sm:col-span-6'>
                         <ProjectCard project={project} key={project.name}/>
                     </div>
                 ))
